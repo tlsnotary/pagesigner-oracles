618075c674f6397d48d9b8531fc1db637d7ad7a2
themighty1 <themighty1>
2016-12-03 12:43:56 +0300

    aws_query: produce copy-pastable output

--- aws_query.py
+++ aws_query.py
@@ -18,7 +18,7 @@ volume_id = sys.argv[2]
 key = sys.argv[3]
 secret = sys.argv[4]
 
-def makeurl(args, endpoint):
+def makeurl(args, endpoint, abbr):
     args.sort()
     argstr = ''
     for arg in args:
@@ -28,8 +28,7 @@ def makeurl(args, endpoint):
     base64str = base64.b64encode(mhmac.digest()).strip().decode('utf-8')
     urlenc_sig = urllib.quote_plus(base64str)
     final_string='https://'+endpoint+'/?'+argstr+'&Signature='+urlenc_sig
-    print ('')
-    print final_string
+    print ("'" + abbr + "':'" + final_string + "',")
 
 
 args = []
@@ -38,7 +37,7 @@ args.append('Action=DescribeInstances')
 args.append('InstanceId='+instance_id)
 args.append('AWSAccessKeyId='+key)
 args.append('Version=2014-10-01')
-makeurl(args, availability_zone)
+makeurl(args, availability_zone, 'DI')
 
 args = []
 args.extend(common_args)
@@ -46,7 +45,7 @@ args.append('Action=DescribeVolumes')
 args.append('VolumeId='+volume_id)
 args.append('AWSAccessKeyId='+key)
 args.append('Version=2014-10-01')
-makeurl(args, availability_zone)
+makeurl(args, availability_zone, 'DV')
 
 args = []
 args.extend(common_args)
@@ -54,14 +53,14 @@ args.append('Action=GetConsoleOutput')
 args.append('InstanceId='+instance_id)
 args.append('AWSAccessKeyId='+key)
 args.append('Version=2014-10-01')
-makeurl(args, availability_zone)
+makeurl(args, availability_zone, 'GCO')
 
 args = []
 args.extend(common_args)
 args.append('Action=GetUser')
 args.append('AWSAccessKeyId='+key)
 args.append('Version=2010-05-08')
-makeurl(args, 'iam.amazonaws.com')
+makeurl(args, 'iam.amazonaws.com', 'GU')
 
 args = []
 args.extend(common_args)
@@ -70,4 +69,4 @@ args.append('InstanceId='+instance_id)
 args.append('Attribute=userData')
 args.append('AWSAccessKeyId='+key)
 args.append('Version=2014-10-01')
-makeurl(args, availability_zone)
+makeurl(args, availability_zone, 'DIA')
